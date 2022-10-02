3abd6d45fd8a66934041f49786ef965123b5682b
ahmedayman1420 <76254195+ahmedayman1420@users.noreply.github.com>
2022-10-02 22:29:43 +0200

    back-end setup

---
+++ Server/modules/users/routes/user-routes.js
@@ -0,0 +1,31 @@
+// ====== --- ====== > Import Modules & Variables Declaration < ====== --- ====== //
+const express = require("express");
+const router = express.Router();
+const userFunctions = require("../conroller/user-control");
+const userSchemas = require("../joi/user-joi");
+const validateRequest = require("../../../Common/Middlewares/requestValidation");
+const userEndpoints = require("../endpoints");
+const isAuthorized = require("../../../Common/Middlewares/isAuthorized");
+/*
+//==// require express to create sub-object that will used to contains user apis
+//==// userFunctions: it's an object that contains all user apis logic
+//==// userSchemas: it's an object that contains all user apis schemas
+//==// validateRequest: it's a function that used validate schema with sent request
+*/
+
+// ====== --- ====== > User Routes < ====== --- ====== //
+
+// Hello dev api
+router.get("/", (req, res) => {
+  res.send("Hello my best developer, all is well!");
+});
+
+// update api
+// router.post(
+//   "/user-update-password",
+//   validateRequest(userSchemas.updatePasswordSchema),
+//   isAuthorized(userEndpoints.updateUserPassword),
+//   userFunctions.updatePassword
+// );
+// ====== --- ====== > Export Module < ====== --- ====== //
+module.exports = router;
